3c307132f80085901c39f8f957af7d6de1f164e9
Athena <atraya@csumb.edu>
2020-02-11 22:52:40 -0800

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Athena Raya on 2/10/20.
+//  Copyright © 2020 Athena Raya. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
